3145908c7f062994d89147ca2f7533b7d769d5d2
gcxfd <i@rmw.link>
2021-04-22 01:18:25 +0800

    🔶

--- docker/push.sh
+++ docker/push.sh
@@ -4,8 +4,8 @@ set -e
 DIR=$(cd "$(dirname "$0")/.."; pwd)
 cd $DIR
 
-./docker/build.sh
-
 NAME=$(basename $DIR)
 
-docker push $DOCKER_HOST/$DOCKER_REPO/$NAME:latest
+DOCKER_NAME=$DOCKER_HOST/$DOCKER_REPO/$NAME:`date +"%Y-%m-%d"`
+docker tag $NAME $DOCKER_NAME
+docker push $DOCKER_NAME
